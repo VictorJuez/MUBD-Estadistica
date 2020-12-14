dbb5958103be2d93ed9b942ca209797d4103fe86
Victor <victorjuez.alu@gmail.com>
2020-12-14 17:27:32 +0100

    kmeans 1st version

--- Practica2-Clustering/script.R
+++ Practica2-Clustering/script.R
@@ -1,17 +1,96 @@
-library(NbClust)
-library(clValid)
+library(scatterplot3d)  
 library(flexclust)
+library(NbClust)        # NbClust
 library(cluster)
-library(ggplot2)
+library(factoextra)     # fviz_***
+library(kernlab)        # kkmeans
+library(clValid)        # clValid
+library(cluster)        # pam
+library(Amelia)
 
 datos <- read.table('Datos de entrenamiento.txt',header=TRUE,sep='\t', dec = '.')
-datos$subject = NULL;
-datos$activity = NULL;
+datos = datos[1:600,]
 
-datos = datos[1:100,]
+datos2 = datos;
+datos2$subject = NULL;
+datos2$activity = NULL;
+datos2 = scale(datos2)
 
 summary(datos)
 head(datos)
+
+## Regla del codo
+VE <- c()
+for (k in 1:10){
+  km <- kmeans(datos2,centers=k,nstart=10)
+  VE[k] <- km$betweenss/km$totss       
+}
+plot(VE,type="b",pch=19,xlab="Numero de grupos",ylab="Variabilidad explicada")
+## K = 3
+
+
+## ACP
+#missing(datos)
+#cor(datos)
+cli = prcomp(datos2, scale=TRUE, center=TRUE)
+#plot(cli)
+principales = cli$x
+principales = principales[,1:50]
+
+# Analisi de clusters
+set.seed(12345)
+ncluster = NbClust(principales, min.nc=2, max.nc=10, method="kmeans")
+ncluster
+par(mfrow=c(1,1))
+barplot(table(ncluster$Best.n[1,]))
+# k=3
+
+
+############################################################
+# Representacion grafica
+############################################################
+##-- 3 grupos
+km3 <- kmeans(datos2,centers=3,nstart=10)
+ve3 <- km3$betweenss/km3$totss
+
+##-- En las 2 primeras componentes
+pr.comp <- princomp(datos2)
+x <- pr.comp$scores[,1]
+y <- pr.comp$scores[,2]
+plot(x,y,pch=19,col=km3$cluster)
+
+## -- 4 grupos
+km4 <- kmeans(datos2,centers=4,nstart=10)
+
+## -- 6 grupos (solo para verificar con enunciado)
+km6 <- kmeans(datos2,centers=6,nstart=10)
+
+##-- En las 2 primeras componentes
+pr.comp <- princomp(datos2)
+x <- pr.comp$scores[,1]
+y <- pr.comp$scores[,2]
+plot(x,y,pch=19,col=km4$cluster)
+
+# Nos quedamos con 3 clusters
+
+############################################################
+# Similitud con etiquetas iniciales
+############################################################
+randIndex(table(km3$cluster,datos$activity))
+
+########################################################################################################################
+########################################################################################################################
+########################################################################################################################
+########################################################################################################################
+########################################################################################################################
+########################################################################################################################
+
+# Extra
+#######
+# Escalamos los datos
+minValue = min(datos) * -1
+datos2 = scale(log(datos+ minValue + 1))
+
 
 ## Imputacion multiple para los datos ausentes
 #library(mice)
@@ -29,68 +108,3 @@ cli = prcomp(datos, scale=TRUE, center=TRUE)
 principales = cli$x
 principales = principales[,1:50]
 #head(principales)
-
-## Regla del codo
-VE <- c()
-for (k in 1:10){
-  km <- kmeans(principales,centers=k,nstart=10)
-  VE[k] <- km$betweenss/km$totss       
-}
-plot(VE,type="b",pch=19,xlab="Numero de grupos",ylab="Variabilidad explicada")
-
-# Analisi de clusters
-library(NbClust)
-set.seed(12345)
-ncluster = NbClust(principales, min.nc=2, max.nc=10, method="kmeans")
-ncluster
-par(mfrow=c(1,1))
-barplot(table(ncluster$Best.n[1,]))
-# k=3
-
-
-
-#######
-# Escalamos los datos
-minValue = min(datos) * -1
-datos2 = scale(log(datos+ minValue + 1))
-
-DUN = SIL = matrix(nrow=6, ncol=4)
-set.seed(12345)
-D = dist(datos2)
-for (i in 3:8){
-  ##-- Clusterizacion jerarquica para obtener los centros
-  hc <- hclust(D,method = 'ward.D') 
-  cl <- cutree(hc,i)
-  centers <- apply(datos2,2,tapply,cl,mean)
-  
-  ##-- 4 metodos
-  km_1 <- kmeans(datos2,i,algorithm='Lloyd',nstart=1)       # Metodo 1
-  km_2 <- kmeans(datos2,i)                                  # Metodo 2
-  km_3 <- kmeans(datos2,i,nstart=10)                        # Metodo 3
-  km_4 <- kmeans(datos2,centers=centers)                    # Metodo 4
-  
-  ##-- Calcular indices y guardarlos en una matrix
-  DUN[i-2,] <- c(dunn(D,km_1$cluster),
-                 dunn(D,km_2$cluster),
-                 dunn(D,km_3$cluster),
-                 dunn(D,km_4$cluster))
-  SIL[i-2,] <- c(summary(silhouette(km_1$cluster,D))[[4]],
-                 summary(silhouette(km_2$cluster,D))[[4]],
-                 summary(silhouette(km_3$cluster,D))[[4]],
-                 summary(silhouette(km_4$cluster,D))[[4]])
-  
-  print(DUN)
-  print(SIL)
-  print('-----------------------')
-  
-}
-round(DUN,3)
-m = which.max(DUN)
-arrayInd(m, .dim = dim(DUN))
-# Nos da el mejor resultado utilizando 8 clusters y el algoritmo de Lloyd
-
-round(SIL,3)
-m2 = which.max(SIL)
-arrayInd(m2, .dim = dim(SIL))
-# Nos da el mejor resultado utilizando 3 clusters y el metodo 3
-
